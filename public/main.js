66c880cabdfee144005e87c966258e9c1012fb2c
wbmmusic <wbmmusic@gmail.com>
2020-12-24 16:00:24 -0600

    0.0.108

--- public/main.js
+++ public/main.js
@@ -138,8 +138,8 @@ app.on('ready', () => {
       autoUpdater.on('checking-for-update', () => win.webContents.send('checkingForUpdates'))
       autoUpdater.on('update-available', () => win.webContents.send('updateAvailable'))
       autoUpdater.on('update-not-available', () => win.webContents.send('noUpdate'))
-      autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => win.webContents.send('updateDownloaded', releaseNotes, releaseName))
-      autoUpdater.on('download-progress', (progress) => win.webContents.send('message', 'Download Progress'))
+      autoUpdater.on('update-downloaded', (releaseNotes, releaseName) => win.webContents.send('updateDownloaded', releaseNotes, releaseName))
+      autoUpdater.on('download-progress', () => win.webContents.send('message', 'Download Progress'))
       autoUpdater.on('error', message => win.webContents.send('updateError', message))
 
 
