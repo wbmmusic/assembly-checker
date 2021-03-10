d2345322d6236e2833d03c90213ebda4068b22fd
wbmmusic <wbmmusic@gmail.com>
2021-03-09 20:26:32 -0600

    0.0.165

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, useHistory } from 'react-router-dom'
+import { Route, Switch, useHistory, useLocation } from 'react-router-dom'
 import Modals from './Modals'
 import SelectDevice from './SelectDevice'
 import Device from './Device'
@@ -7,14 +7,15 @@ import Device from './Device'
 
 export default function Top(props) {
     const history = useHistory()
-    console.log('LOACTION ->', history.location.pathname)
+    const location = useLocation()
+    console.log('History ->', history.location.pathname)
+    console.log('Location ->', location.pathname)
 
     return (
         <div style={{ height: '100%' }}>
             <Switch>
-                
+                <Route path="/" exact component={SelectDevice} />
                 <Route path="/device" component={Device} />
-                <Route  component={SelectDevice} />
             </Switch>
             <Modals />
         </div>
